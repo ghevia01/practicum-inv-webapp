97178e7bf603b8f32b09db5ab6a2194038b570bf
Gean Hevia <gcaarlos30@gmail.com>
2023-10-29 17:57:03 -0400

    feat: added function to toggle section visibility

--- javascript/utils/toggleQrScannerUI.js
+++ javascript/utils/toggleQrScannerUI.js
@@ -1,9 +1,24 @@
-import { qrScannerIcon, scanButton, stopButton, video } from "./qrPageElements.js";
+import {
+  qrScannerIcon,
+  scanButton,
+  stopButton,
+  video,
+} from "./qrPageElements.js";
 
 // Toggle the UI between the scanning and the stopped states
-export function toggleQRScannerUI(isScanning) {
+export const toggleQRScannerUI = (isScanning) => {
   scanButton.classList.toggle("hidden", isScanning);
   qrScannerIcon.classList.toggle("hidden", isScanning);
   stopButton.classList.toggle("hidden", !isScanning);
   video.classList.toggle("hidden", !isScanning);
-}
+}; 
+
+// Toggle the visibility of a section
+export const toggleSectionVisibility = (section, isVisible) => {
+  if (isVisible) {
+    section.classList.remove("hidden");
+    section.setAttribute("aria-hidden", "false");
+  } else {
+    section.classList.add("hidden");
+  }
+};
