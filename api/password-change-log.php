5fc1eb13527e98ca24ffb3f29bd423fe7f8a337d
jacinto126 <jacintopr126@gmail.com>
2023-11-12 14:38:29 -0400

    login working

--- api/password-change-log.php
+++ api/password-change-log.php
@@ -1,4 +1,21 @@
 <?php
 
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "phpmyadmin" ;
+
+//connector
+$con= mysqli_connect($serverName,$userName,$password,$dbName);
+
+if(mysqli_connect_errno()){
+    echo "Failed Connection!";
+    exit();
+}
+echo "Connection success !";
+
+
+
+
 
 ?>
