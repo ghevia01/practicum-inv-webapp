4d17316339b83a9ecf096f72ae05ab02f5f36766
jacinto126 <jacintopr126@gmail.com>
2023-11-13 17:21:49 -0400

    login done ish

--- api/password-change-log.php
+++ api/password-change-log.php
@@ -1,18 +1,52 @@
 <?php
+$email = $_POST["email"];
 
-$serverName = "localhost";
-$userName = "root";
-$password = "";
-$dbName = "phpmyadmin" ;
+$token = bin2hex(random_bytes(16));
 
-//connector
-$con= mysqli_connect($serverName,$userName,$password,$dbName);
+$token_hash = hash("sha256", $token);
 
-if(mysqli_connect_errno()){
-    echo "Failed Connection!";
-    exit();
+$expiry = date("Y-m-d H:i:s", time() + 60 * 30);
+
+$mysqli = require __DIR__ . "/database.php";
+
+$sql = "UPDATE logintable
+        SET reset_token_hash = ?,
+            reset_token_expires_at = ?
+        WHERE email = ?";
+
+$stmt = $mysqli->prepare($sql);
+
+$stmt->bind_param("sss", $token_hash, $expiry, $email);
+
+$stmt->execute();
+
+if ($mysqli->affected_rows) {
+
+    $mail = require __DIR__ . "/mailer.php";
+
+    $mail->setFrom("noreply@example.com");
+    $mail->addAddress($email);
+    $mail->Subject = "Password Reset";
+    $mail->Body = <<<END
+
+    Click <a href="http://example.com/reset-password.php?token=$token">here</a> 
+    to reset your password.
+
+    END;
+
+    try {
+
+        $mail->send();
+
+    } catch (Exception $e) {
+
+        echo "Message could not be sent. Mailer error: {$mail->ErrorInfo}";
+
+    }
+
 }
-echo "Connection success !";
+
+echo "Message sent, please check your inbox.";
 
 
 
