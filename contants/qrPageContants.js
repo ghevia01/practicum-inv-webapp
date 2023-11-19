7e6b8a817aa0b09c1d6fc9682e998b2b9887f8a6
Gean Hevia <gcaarlos30@gmail.com>
2023-11-19 17:55:33 -0400

    Add qrPageContants.js file with initial constants

---
+++ contants/qrPageContants.js
@@ -0,0 +1,30 @@
+export const keysToShow = [
+  "Ptag",
+  "Description",
+  "Model",
+  "Serial_No",
+  "Location",
+  "Status",
+  "Comments",
+];
+
+// Create location options
+export const locationOptions = [
+  "E-169",
+  "E-170",
+  "E-249A",
+  "E-249B",
+  "E-249C",
+  "F-104",
+  "F-201",
+  "F-202",
+  "F-203",
+  "F-209",
+  "F-210",
+];
+
+export const readInventoryURL =
+  "http://localhost/practicum-inv-webapp/api/read-inventory.php";
+
+export const updateInventoryURL =
+  "http://localhost/practicum-inv-webapp/api/update-inventory.php";
