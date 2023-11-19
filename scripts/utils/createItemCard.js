7bc2ea3e34f72e5af6dc3164de5d13b03a58da36
Gean Hevia <gcaarlos30@gmail.com>
2023-11-19 17:40:21 -0400

    Refactor createItemCard function

--- scripts/utils/createItemCard.js
+++ scripts/utils/createItemCard.js
@@ -1,49 +1,76 @@
-export const createItemCard = (itemData) => {
-  // Define keys to show in the card
-  const keysToShow = [
-    "Ptag",
-    "Description",
-    "Model",
-    "Serial_No",
-    "Location",
-    "Status",
-    "Comments",
-  ];
+import { handleEditClick } from "../eventHandlers/itemCardHandlers.js";
+import { createEditIcon, createCancelIcon, createSelect } from "./domUtils.js";
+import { keysToShow } from "../../contants/qrPageContants.js";
 
-  // Create elements
+export const createItemCard = (fetchedItemData) => {
+  // Create the card element
   const card = document.createElement("div");
-  const cardHeader = document.createElement("div");
-  const cardHeaderTitle = document.createElement("h2");
-  const cardBodyList = document.createElement("dl");
-
-  // Assign classes to elements
   card.className = "result-card";
+
+  // Create the card header element and its title
+  const cardHeader = document.createElement("div");
   cardHeader.className = "result-title";
+  const cardHeaderTitle = document.createElement("h2");
   cardHeaderTitle.textContent = "Scan Results";
-
-  // Append elements to cardHeader
   cardHeader.appendChild(cardHeaderTitle);
-
-  // Append card header to card
   card.appendChild(cardHeader);
 
-  // Create and append elements to cardBodyList
+  // Create the list to display the data
+  const cardBodyList = document.createElement("dl");
+  card.appendChild(cardBodyList);
+
+  // Iterate over each key to display its value
   keysToShow.forEach((key) => {
-    if (itemData[key]) {
-      const dt = document.createElement("dt");
-      dt.textContent = key.replace(/_/g, " ");
-      const dd = document.createElement("dd");
+    // Create the key element (field name)
+    const dt = document.createElement("dt");
+    dt.textContent = key.replace(/_/g, " ");
 
-      dt.textContent = key;
-      dd.textContent = itemData[key] !== undefined ? itemData[key] : "N/A";
+    // Create the value element (field value)
+    const dd = document.createElement("dd");
+    dd.setAttribute("data-key", key);
+    dd.textContent = fetchedItemData[key] !== undefined ? fetchedItemData[key] : "N/A";
 
+    if (key === "Location") {
+      // Create location options
+      const locationOptions = ["F-201", "F-202", "F-203", "F-204"];
+
+      // Create a copy of fetchedItemData for this card
+      const itemData = { ...fetchedItemData };
+
+      // Create a container for dd and the edit icon
+      const ddContainer = document.createElement("div");
+      ddContainer.className = "dd-container";
+
+      // Create and populate the select element
+      const select = createSelect(locationOptions, itemData, key);
+
+      // Create cancel icon
+      const cancelIcon = createCancelIcon(key);
+
+      console.log("Select element:", select);
+
+      // Create edit icon and add event listener
+      const editIcon = createEditIcon(key);
+      editIcon.addEventListener("click", (event) =>
+        handleEditClick(event, itemData, key, select)
+      );
+
+      // Append dd element to the container
+      ddContainer.appendChild(dd);
+
+      // Append editIcon icon and cancel icon
+      ddContainer.appendChild(editIcon);
+      ddContainer.appendChild(cancelIcon);
+
+      // Append container to the list
+      cardBodyList.appendChild(dt);
+      cardBodyList.appendChild(ddContainer);
+    } else {
+      // Append key and value to list
       cardBodyList.appendChild(dt);
       cardBodyList.appendChild(dd);
     }
   });
-
-  // Append elements to card
-  card.appendChild(cardBodyList);
 
   return card;
 };
