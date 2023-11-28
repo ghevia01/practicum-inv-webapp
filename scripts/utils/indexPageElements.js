858f1eb08153b8e965e5cb10f628d33ba1420731
Gean Hevia <gcaarlos30@gmail.com>
2023-11-27 20:09:46 -0400

    feat: add new button for reports page

--- scripts/utils/indexPageElements.js
+++ scripts/utils/indexPageElements.js
@@ -1,3 +1,4 @@
 // Get main page elements
 export const qrScannerPageBtn = document.getElementById('qrScannerPageButton');
 export const manageUsersPageBtn = document.getElementById('manageUsersPageButton');
+export const reportsPageBtn = document.getElementById('reportsPageButton');
